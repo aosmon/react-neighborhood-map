244296063f5df5d5734f9f86bd4ff881b20f432f
Aizhan Osmonova <osmonova86@gmail.com>
2018-08-26 17:41:28 -0400

    feat: Implement search venues functionality

--- src/ListVenues.js
+++ src/ListVenues.js
@@ -2,14 +2,50 @@ import React, {Component} from 'react'
 
 class ListVenues extends Component {
 
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState(()=>({
+			query: query.trim()
+		}))
+	}
+
+	clearQuery = () => {
+		this.updateQuery('')
+	}
+
 	render(){
+		const { query } = this.state
+		const { venues} = this.props
+
+		const showingVenues = query === ''
+			? venues
+			: venues.filter((v) => (
+				v.name.toLowerCase().includes(query.toLowerCase())
+			))
+
 		return (
 			<section className='venues-list-container'>
 				<div className='list-venues-top'>
-
+					<input className='search-values'
+						type='text'
+						placeholder='Search Venues'
+						value={query}
+						onChange={(event)=> this.updateQuery(event.target.value)}
+					/>
 				</div>
+
+				{showingVenues.length !== venues.length && (
+					<div className='showing-venues'>
+						<span>Now showing {showingVenues.length} of {venues.length}</span>
+						<button onClick={this.clearQuery}> Show all </button>
+					</div>
+				)}
+
 				<ul className="venues-list">
-					{this.props.venues.map((venue)=>(
+					{showingVenues.map((venue)=>(
 						<li key='venue.id' className='venue-list-item'>
 							{venue.name}
 						</li>
